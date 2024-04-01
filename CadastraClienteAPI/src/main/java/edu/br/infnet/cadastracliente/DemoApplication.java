766068a61a2156243512235762d9a34912335adb
eduardoluiznatividade <eduardo.natividade@al.infnet.edu.br>
2024-04-01 20:15:25 -0300

    commit inicial do projeto cadastra cliente api

---
+++ CadastraClienteAPI/src/main/java/edu/br/infnet/cadastracliente/DemoApplication.java
@@ -0,0 +1,13 @@
+package edu.br.infnet.cadastracliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
